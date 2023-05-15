1bde7fbc1c1ffecfeff69d3dc4ae09bfa9c90ec5
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-15 15:11:28 +0200

    Ajout des tags et refonte de la requete

--- backend/src/models/ArticlesManager.js
+++ backend/src/models/ArticlesManager.js
@@ -12,6 +12,17 @@ class ItemManager extends AbstractManager {
     `);
   }
 
+  find(id) {
+    return this.database.query(
+      `select a.id, a.title, a.subtitle, a.resume, a.text, a.author, i.src, i.alt, JSON_ARRAYAGG(t.label) as tags from ${this.table} as a
+      inner join images as i on i.id = a.image_id
+      inner join article_to_tags as att on att.article_id = a.id
+      inner join tags as t on t.id = att.tags_id
+      where a.id = ?`,
+      [id]
+    );
+  }
+
   insert(item) {
     return this.database.query(`insert into ${this.table} (title) values (?)`, [
       item.title,
