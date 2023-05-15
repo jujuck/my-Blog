ccf7147b6b2a22f1d9a569934836c096c77b69a1
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-15 14:30:42 +0200

    Mise en place des routes articles

--- backend/src/models/ArticlesManager.js
+++ backend/src/models/ArticlesManager.js
@@ -2,7 +2,7 @@ const AbstractManager = require("./AbstractManager");
 
 class ItemManager extends AbstractManager {
   constructor() {
-    super({ table: "item" });
+    super({ table: "article" });
   }
 
   insert(item) {
