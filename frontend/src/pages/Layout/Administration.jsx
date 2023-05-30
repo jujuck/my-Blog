da2d3144f04f4309d9f19b3f0292676e22a02efd
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-30 15:00:40 +0200

    Mise en place de la sécurisation de la route front end admin

--- frontend/src/pages/Layout/Administration.jsx
+++ frontend/src/pages/Layout/Administration.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useCurrentUser } from "../../contexts/UserContexts";
 
 function Administration() {
-  return (
-    <div className="container-fluid">
-      <div className="row flex-fill">
-        <nav className="col-2 bg-secondary vh-100">
-          <h3 className="text-center text-white m-4">Menu</h3>
-        </nav>
-        <div className="col-9">
-          <Outlet />
+  const { user } = useCurrentUser();
+  if (user === "Connected") {
+    return (
+      <div className="container-fluid">
+        <div className="row flex-fill">
+          <nav className="col-2 bg-secondary vh-100">
+            <h3 className="text-center text-white m-4">Menu</h3>
+          </nav>
+          <div className="col-9">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <Navigate to="/" replace />;
 }
 
 export default Administration;
